65fb28c3ddeb4bcb8daa7da8eae09eea30372529
Divya S <divya.s@cprime.com>
2025-09-17 12:56:51 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/authentik-webhook", methods=["GET"])
+@app.route("/", methods=["GET"])
+def getServerStatus():
+    return jsonify({"status": "ok","message" : "Server Running..."}), 200
+
+@app.route("/authentik-webhook", methods=["POST"])
 def handle_authentik_event():
     data = request.get_json(force=True, silent=True)  # parse JSON safely
     print("Received webhook payload:", data)
     return jsonify({"status": "ok"}), 200
+
 
 
 if __name__ == "__main__":
